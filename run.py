ed017751228f99a9c2c1b4d53c2a705dfc347bb4
赖信涛 <laixintao1995@163.com>
2015-10-22 13:57:40 +0800

    run.py log()

--- run.py
+++ run.py
@@ -4,12 +4,19 @@ import os
 from moviedb import create_table
 from LFMSpider import LFMSpider
 from config import MOVIE_DB_PATH
+from log import log
 
 if __name__ == "__main__":
-    if os.path.exists(MOVIE_DB_PATH):
-        pass
-    else:
-        create_table()
-    lfmspider = LFMSpider()
-    lfmspider.parser_all_pages(0)
+    try:
+        log("-"*20+"RUN"+"-"*20)
+        if os.path.exists(MOVIE_DB_PATH):
+            pass
+        else:
+            create_table()
+        lfmspider = LFMSpider()
+        lfmspider.parser_all_pages(0)
+    except Exception,e:
+        log(str(e))
+    finally:
+        log("-"*20+"END"+"-"*20)
 
